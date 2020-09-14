76f6744001a679b505fd2f44dd2496998a8ce6c0
Lenni Korhonen <lenni@pop-os.localdomain>
2020-09-14 13:02:53 +0300

    Infinitelly running bt scan

--- python_bt/bt_beacon.py
+++ python_bt/bt_beacon.py
@@ -1,8 +1,19 @@
 import bluetooth
+import schedule
+import time
 
-devices = bluetooth.discover_devices(lookup_names=True)
-print(type(devices))
-print("Devices found: %s" % len(devices))
 
-for item in devices:
-    print(item)
+def scan():
+    devices = bluetooth.discover_devices(lookup_names=True)
+    print(type(devices))
+    print("Devices found: %s" % len(devices))
+
+    for item in devices:
+        print(item)
+
+
+schedule.every(2).seconds.do(scan)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
