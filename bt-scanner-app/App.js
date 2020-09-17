03ccc879527bfc174fdcfa151245f155c11a1e3a
Roope <roope.laakso@gmail.com>
2020-09-17 20:28:24 +0300

    add different colors to markers

--- bt-scanner-app/App.js
+++ bt-scanner-app/App.js
@@ -1,40 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import axios from 'axios'
 
 export default function App() {
   const [places, setPlaces] = useState([])
 
+  const giveRandomNumber = () => {
+    return String(Math.floor(Math.random() * 101))
+  }
+
+  const pickPinColor = (bt_connections) => {
+    if (bt_connections < 25) {
+      return 'green'
+    } else if (bt_connections < 50) {
+      return 'yellow'
+    } else {
+      return 'red'
+    }
+  }
+
   useEffect(() => {
     axios
       .get('http://open-api.myhelsinki.fi/v1/places/')
       .then(res => {
-        console.log('data: ', res.data.data)
+        // console.log('data: ', res.data.data)
         setPlaces(res.data.data.slice(0, 50))
       })
   }, [])
 
+  const handleMarkerPress = (event) => {
+    console.log(event)
+  }
+
   return (
-    <MapView style={styles.container}
-      initialRegion={{
-        latitude: 60.2997901,
-        longitude: 25.0592432,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-    >
-      {places.length !== 0
-        ? places.map(place => (
-          <Marker 
-            coordinate={{
-              latitude: place.location.lat,
-              longitude: place.location.lon
-            }}
-          />
-        ))
-        : null}
-    </MapView>
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle}
+        initialRegion={{
+          latitude: 60.2997901,
+          longitude: 25.0592432,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      >
+        {places.length !== 0
+          ? places.map(place => (
+            <Marker
+              onPress={handleMarkerPress}
+              key={place.id}
+              title={place.name.fi}
+              pinColor={pickPinColor(giveRandomNumber())}
+              // description={giveRandomNumber()}
+              coordinate={{
+                latitude: place.location.lat,
+                longitude: place.location.lon
+              }}
+            />
+          ))
+          : null}
+      </MapView>
+    </View>
+    
   );
 }
 
@@ -45,4 +71,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
