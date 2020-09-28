47345348a0d053ed3c513b44b25be13b1323cd83
lennikorhonen <lenni.korhonen2@gmail.com>
2020-09-28 12:14:26 +0300

    Timestamp for scanner

--- python_bt/bt_scanner.py
+++ python_bt/bt_scanner.py
@@ -1,8 +1,8 @@
 import bluetooth
 import schedule
 import time
+import calendar
 import json
-import urllib3
 import requests
 
 
@@ -10,7 +10,7 @@ def scan():
     bt_devices = []
     print('Looking for bluetooth devices...')
     devices = bluetooth.discover_devices(lookup_names=True)
-    # print(type(devices))
+    print(type(devices))
     print("Devices found: %s" % len(devices))
 
     for item in devices:
@@ -22,14 +22,20 @@ def scan():
         else:
             bt_devices.append(item)
 
-    print(bt_devices)
-    json_data = json.dumps(bt_devices)  # .encode('utf-8')
+    # print(bt_devices)
+    json_data = json.dumps(bt_devices)
     print(json_data)
 
+    ts = time.time()
+    readable = time.ctime(ts)
+    print(readable)
+
+
+"""
     url = 'https://raspberrybackend.herokuapp.com/results'
     objToSend = {'location': 'LenninKone', 'foundDevices': len(
-        bt_devices)*3, 'macaddr': '00:0a:95:9d:69:69'}
+        bt_devices)*3, 'macaddr': '00:0a:95:9d:69:69', 'timestamp': ts}
 
     send = requests.post(url, json=objToSend)
 
-    print(send.text)
+    print(send.text)"""
