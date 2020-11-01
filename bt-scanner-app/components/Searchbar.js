0a2b180798101147bd9ccf8edf550df598c72d4d
Roope <roope.laakso@gmail.com>
2020-11-01 13:26:39 +0200

    add component for searching

---
+++ bt-scanner-app/components/Searchbar.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { SearchBar } from 'react-native-elements'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const Searchbar = () => {
+  const [search, setSearch] = useState('')
+
+  const updateSearch = (search) => {
+    console.log(search)
+    setSearch({ search })
+  }
+
+  return (
+    <View style={styles.searchBar}>
+      <SearchBar
+        // style={styles.searchBar}
+        placeholder="Type here..."
+        onChangeText={updateSearch}
+        value={search}
+      />
+    </View>
+      
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  // searchBar: {
+  //   position: 'absolute',
+  //   top: 50,
+  //   left: 50,
+  //   zIndex: 1
+  // }
+})
+
+export default Searchbar
