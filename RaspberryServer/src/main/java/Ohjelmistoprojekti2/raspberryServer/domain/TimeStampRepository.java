44935335e08fe34f0737d0aa609eb07245229f86
lennikorhonen <lenni.korhonen2@gmail.com>
2020-11-02 11:53:57 +0200

    removed two files

--- RaspberryServer/src/main/java/Ohjelmistoprojekti2/raspberryServer/domain/TimeStampRepository.java
+++
@@ -1,9 +0,0 @@
-package Ohjelmistoprojekti2.raspberryServer.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TimeStampRepository extends CrudRepository<TimeStamp, Long>{
-	List<TimeStamp> findByTime(String time);
-}
