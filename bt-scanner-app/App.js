8fca5f463f0ffac2be31d104d4100b97cb82433d
rusinainen <bgg268@myy.haaga-helia.fi>
2020-11-16 13:47:44 +0200

    navin korjausyritys

--- bt-scanner-app/App.js
+++ bt-scanner-app/App.js
@@ -1,8 +1,37 @@
+import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, ActivityIndicator, Container, Button} from 'react-native';
+import locationService from './services/locations'
+//Navigaatio
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//Komponentit
 import PlaceInfo from './components/PlaceInfo';
 import Map from './components/Map';
-import locationService from './services/locations'
+import SightList from './components/SightList';
+
+// TO-do vaihda myöhemmin komponentti ja sen nimi, kun navi todettu toimivaksi
+const MapStack = createStackNavigator();
+function MapStackScreen() {
+  return (
+    <MapStack.Navigator>
+      <MapStack.Screen name ="Capital Area Map View" component={ SightList } />
+    </MapStack.Navigator>
+    );
+}
+
+// Paikkojen listaus
+const PlaceListStack = createStackNavigator();
+function ListStackScreen() {
+  return (
+    <PlaceListStack.Navigator>
+    <PlaceListStack.Screen name ="List of Sights" component={ SightList } />
+    </PlaceListStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [locations, setLocations] = useState([])
@@ -18,14 +47,21 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Map 
-        locations={locations} 
-        setSelectedLocation={setSelectedLocation} 
+      <Map
+        locations={locations}
+        setSelectedLocation={setSelectedLocation}
       />
-      <PlaceInfo 
+      <PlaceInfo
         selectedLocation={selectedLocation}
-        setSelectedLocation={setSelectedLocation} 
+        setSelectedLocation={setSelectedLocation}
       />
+      //Tämä kohta kai pitäis järjestellä jotenki järkevästi(?)
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Lista" component={ListStackScreen} />
+        <Tab.Screen name="Kartta" component={MapStackScreen} />
+      </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
