6479e00195f40769c5022a4608d464f610aebbb8
rusinainen <bgg268@myy.haaga-helia.fi>
2020-11-19 13:49:10 +0200

    Icon lisätty naviin

--- bt-scanner-app/App.js
+++ bt-scanner-app/App.js
@@ -6,18 +6,19 @@ import locationService from './services/locations'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 //Komponentit
 import PlaceInfo from './components/PlaceInfo';
 import Map from './components/Map';
 import SightList from './components/SightList';
 import FrontPage from './components/FrontPage';
 
-
+//Etusivu, eli karttanäkymä
 const MapStack = createStackNavigator();
 function MapStackScreen() {
   return (
     <MapStack.Navigator>
-      <MapStack.Screen name ="Kartta" component={ FrontPage } />
+      <MapStack.Screen name ="Map" component={ FrontPage } />
     </MapStack.Navigator>
     );
 }
@@ -27,7 +28,7 @@ const PlaceListStack = createStackNavigator();
 function ListStackScreen() {
   return (
     <PlaceListStack.Navigator>
-    <PlaceListStack.Screen name ="Kohteet" component={ SightList } />
+    <PlaceListStack.Screen name ="List of sights in Helsinki area" component={ SightList } />
     </PlaceListStack.Navigator>
     );
 }
@@ -40,8 +41,22 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Lista" component={ListStackScreen} />
-        <Tab.Screen name="Kartta" component={MapStackScreen} />
+        <Tab.Screen name="Lista" component={ListStackScreen}
+          options={{
+             tabBarLabel: 'List',
+             tabBarIcon: ({ color }) => (
+               <MaterialCommunityIcons name="format-list-bulleted" color={color} size={26} />
+             ),
+          }}
+        />
+        <Tab.Screen name="Kartta" component={MapStackScreen}
+          options={{
+           tabBarLabel: 'Map',
+           tabBarIcon: ({ color }) => (
+             <MaterialCommunityIcons name="map" color={color} size={26} />
+           ),
+          }}
+        />
       </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
@@ -57,5 +72,5 @@ const styles = StyleSheet.create({
     top: '10%',
     left: '2%',
     zIndex: 0
-  }
+  },
 });
